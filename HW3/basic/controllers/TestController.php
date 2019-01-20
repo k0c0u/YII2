bbdfd8c2aecc69eaec05be116c284f49b6778a7e
k0c0u <k0c0u@mail.ru>
2019-01-20 22:51:11 +0300

    add hw3

---
+++ HW3/basic/controllers/TestController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace app\controllers;
+
+use app\components\TestService;
+use app\models\Product;
+use yii\web\Controller;
+
+class TestController extends Controller
+{
+    public function actionIndex()
+    {
+        return \Yii::$app->test->run();
+
+        $model = new Product();
+        $model->id = 11;
+        $model->name = 'First';
+        $model->cost = 123456;
+        $model->validate();
+        $model->getErrors();
+        $model->safeAttributes();
+        $model->activeAttributes();
+
+        return $this->render('index', [
+            'model' => $model,
+        ]);
+    }
+}
