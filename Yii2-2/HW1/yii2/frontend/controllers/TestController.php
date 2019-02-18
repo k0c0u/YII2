765263dcca2aaec3a910126bd7889310f96e4390
k0c0u <k0c0u@mail.ru>
2019-02-18 11:12:24 +0300

    add HW1

---
+++ Yii2-2/HW1/yii2/frontend/controllers/TestController.php
@@ -0,0 +1,16 @@
+<?php
+namespace frontend\controllers;
+use yii\web\Controller;
+/**
+ * Site controller
+ */
+class TestController extends Controller
+{
+    /**
+     * Displays homepage.
+     * @return string
+     */
+    public function actionIndex() {
+        return $this->renderContent('Hello, world');
+    }
+}
